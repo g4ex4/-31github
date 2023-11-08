0fe573f474a485b6b46c6f52fc39685b6b820ea4
ilimbek <ilimbek.meimanbaev00@gmail.com>
2023-11-08 18:41:03 +0600

    Добавьте файлы проекта.

---
+++ #31github/Program.cs
@@ -0,0 +1,23 @@
+﻿#region TwoSum
+int[] result = TwoSum(new int[] { 2, 5, 5, 11 }, 10);
+
+foreach (int i in result)
+{
+    Console.WriteLine(i);
+}
+
+int[] TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length - 1; i++)
+    {
+        for (int j = 0; j < nums.Length - 1; j++)
+        {
+            if (nums[i] + nums[j + 1] == target && i != j + 1)
+            {
+                return new int[] { i, j + 1 };
+            }
+        }
+    }
+    return null;
+}
+#endregion
